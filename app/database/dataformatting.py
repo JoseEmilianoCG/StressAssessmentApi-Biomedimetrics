ddafa3071fda1a46157854195b3f38a9f482bd6d
J.EmiCG <jemilianocalgu@gmail.com>
2024-05-25 00:20:58 -0600

    Commit 2

--- app/database/dataformatting.py
+++ app/database/dataformatting.py
@@ -1,17 +1,12 @@
-import json
 import numpy as np
 
-def format(json_data):
-    with open('prueba3.json', 'r') as fp:
-        data = json.load(fp)
-
+def format(data):
     data_dict = {'bvp':[],'gsr':[]}
 
     for physdataname in ['bvp','gsr']:
-        for item in data[physdataname]:
-            data_dict[physdataname].extend(data[physdataname][item].values())
+        data_dict[physdataname] = data[physdataname]
 
     bvp_data = np.array([float(value) for value in data_dict['bvp']])
     eda_data = np.array([float(value) for value in data_dict['gsr']])
-    
+
     return bvp_data, eda_data
---
+++ app/globalvariables.py
@@ -0,0 +1,6 @@
+def actualizevariables(eda_baseline, hr_baseline, sdsd_baseline, rmssd_baseline):
+    global edaref, hrref, sdsdref, rmssdref
+    edaref = eda_baseline
+    hrref = hr_baseline
+    sdsdref = sdsd_baseline
+    rmssdref = rmssd_baseline
